45382a66f0c8e47859187d807b879984de6781de
Kavix0 <kavix0@gmail.com>
2023-03-11 19:25:23 -0600

    Added button functionality and mousedown listener. Work on size updater

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,53 @@
 const container = document.getElementById('container');
-let currentColor = 'straightBlack';
+const sliderValue = document.getElementById('rangeValue');
+const blackButton = document.getElementById('blackButton');
+const rainbowButton = document.getElementById('rainbowButton');
+const eraserButton = document.getElementById('eraserButton');
 
-let numSquares = (prompt("How many squares would you like in the grid?"));
+let currentColor = 'blackButton';
+let numSquares = sliderValue.value;
+let mouseClicked = false;
+
 renderGrid(numSquares);
 
-function renderGrid(numSquares){
+sliderValue.oninput = function(){
+    numSquares = sliderValue.value;
+}
 
+blackButton.onmousedown = function(){
+    activeButton = document.getElementById(currentColor);
+    activeButton.classList.remove('active')
+
+    blackButton.classList.add('active');
+    currentColor = 'blackButton';
+}
+
+rainbowButton.onmousedown = function(){
+    activeButton = document.getElementById(currentColor);
+    activeButton.classList.remove('active')
+
+    rainbowButton.classList.add('active');
+    currentColor = 'rainbowButton';
+}
+
+eraserButton.onmousedown = function(){
+    activeButton = document.getElementById(currentColor);
+    activeButton.classList.remove('active')
+
+    eraserButton.classList.add('active');
+    currentColor = 'eraserButton';
+}
+
+clearButton.onmousedown = function(){
+    emptyGrid();
+    renderGrid(); 
+}
+
+function emptyGrid(){
+    document.querySelectorAll('.gridElement').forEach(e => e.remove());
+}
+
+function renderGrid(){
     let gridArea = numSquares * numSquares;
 
     container.style.gridTemplateColumns = `repeat(${numSquares}, 1fr)`;
@@ -15,7 +57,8 @@ function renderGrid(numSquares){
         let gridItem = document.createElement('div');
         gridItem.classList.add('gridElement');
         gridItem.addEventListener('mouseover', changeColor);
-        gridItem.addEventListener('mousedown', changeColor);
+        gridItem.addEventListener('mousedown', mouseDown, changeColor);
+        gridItem.addEventListener('mouseup', mouseUp);
         container.appendChild(gridItem);
     }
 
@@ -27,18 +70,18 @@ function renderGrid(numSquares){
     });*/
 }
 
+function mouseDown(e){
+    mouseClicked = true;
+}
+
+function mouseUp(e){
+    mouseClicked = false;
+}
+
 function changeColor(e) {
-    e.target.style.backgroundColor = "#000000";
-}
+    if(!mouseClicked) return;
 
-randomColor = function(e) {
-    e.style.background =  "#" + (Math.random() * 0xFFFFFF<<0).toString(16);
-}
-
-straightBlack = function(e) {
-    e.style.background = "#000000";
-}
-
-shadesOfBlack = function(e) {
-    e.style.background = (0, 0, 0, 0);
+    if(currentColor == 'blackButton') e.target.style.background = "#000000";
+    else if (currentColor == 'rainbowButton') e.target.style.background =  "#" + (Math.random() * 0xFFFFFF<<0).toString(16);
+    else if (currentColor == 'eraserButton') e.target.style.background = "#F4F7F3";
 }
